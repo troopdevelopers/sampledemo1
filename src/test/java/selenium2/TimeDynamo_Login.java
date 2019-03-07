1920992689fe6575b2930dae3c5ec56f3786a43f
troopdevelopers <47349174+troopdevelopers@users.noreply.github.com>
2019-03-07 17:11:03 +0530

    first commit
    first commit

---
+++ src/test/java/selenium2/TimeDynamo_Login.java
@@ -0,0 +1,28 @@
+package selenium2;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TimeDynamo_Login {
+
+	
+	@Test
+	public  void testt() {
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://tvishasystems.com/webdemo/timedynamo_testing/public/login");
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.findElement(By.id("bG9naW5Vc2VybmFtZUVsZW1lbnQ")).sendKeys("admin@tvisha.com");
+		driver.findElement(By.id("bG9naW5QYXNzd29yZEVsZW1lbnQ")).sendKeys("Admin@1201@");
+		driver.findElement(By.id("bG9naW5CdG4")).click();
+		
+	}
+}
