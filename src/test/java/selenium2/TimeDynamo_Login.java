9209e9336ab4fd61338326b25c477f48abdc35cf
troopdevelopers <47349174+troopdevelopers@users.noreply.github.com>
2019-03-11 18:31:57 +0530

    Update TimeDynamo_Login.java

--- src/test/java/selenium2/TimeDynamo_Login.java
+++ src/test/java/selenium2/TimeDynamo_Login.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
